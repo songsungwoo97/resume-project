3590061201a36a6a6032ec27922022c6a212b5e6
songsungwoo <sqwa9200@naver.com>
2025-02-01 23:16:10 +0900

    a4page

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -6,7 +6,7 @@ import { Footer } from "./Footer";
 
 export const Resume = () => {
   return (
-    <div className="w-[210mm] h-[297mm] mx-auto bg-white p-4 shadow-lg">
+    <div className="w-[210mm] min-h-[297mm] max-h-[297mm] mx-auto bg-white p-8 shadow-lg">
       <Header />
       <h1 className="text-2xl font-bold text-center mb-4">이 력 서</h1>
       <PersonalInfo />
